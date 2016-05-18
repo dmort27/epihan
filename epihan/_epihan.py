6917ea18e4b3851279e78fb378d13c037b010b0d
David R. Mortensen <davidmortensen@gmail.com>
2016-05-18 15:51:09 -0400

    Added _epihan.py

---
+++ epihan/_epihan.py
@@ -0,0 +1,45 @@
+# -*- utf-8 -*-
+from __future__ import print_function, unicode_literals
+
+import cedict
+import rules
+
+
+class Normalizer(object):
+
+    def normalize_punc(self, text):
+        punc = [(u'\uff0c', u','),
+                (u'\uff01', u'!'),
+                (u'\uff1f', u'?'),
+                (u'\uff1b', u';'),
+                (u'\uff1a', u':'),
+                (u'\uff08', u'('),
+                (u'\uff09', u')'),
+                (u'\uff3b', u'['),
+                (u'\uff3d', u']'),
+                (u'\u3010', u'['),
+                (u'\u3011', u']'),
+                ]
+        for a, b in punc:
+            text = text.replace(a, b)
+        return text
+
+
+class Epihan(Normalizer):
+    def __init__(self, cedict_file, rules_file):
+        self.cedict = cedict.CEDictTrie(cedict_file)
+        self.rules = rules.Rules([rules_file])
+
+    def transliterate(self, text, normpunc=False):
+        tokens = self.cedict.tokenize(text)
+        ipa_tokens = []
+        for token in tokens:
+            if token in self.cedict.hanzi:
+                (pinyin, _) = self.cedict.hanzi[token]
+                ipa = self.rules.apply(pinyin)
+                ipa_tokens.append(ipa.replace(',', ''))
+            else:
+                if normpunc:
+                    token = self.normalize_punc(token)
+                ipa_tokens.append(token)
+        return ''.join(ipa_tokens)
