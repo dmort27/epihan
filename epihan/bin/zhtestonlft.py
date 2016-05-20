e175b3ebeeffe1d4bb59d9fdcad2b79930a7f80c
David R. Mortensen <dmortens@cs.cmu.edu>
2016-05-19 21:25:23 -0400

    Corrected zhtestonlft.py errors

--- epihan/bin/zhtestonlft.py
+++ epihan/bin/zhtestonlft.py
@@ -4,16 +4,21 @@ from __future__ import print_function, unicode_literals
 import epihan.vector
 import lxml.etree as etree
 import argparse
+import types
+import codecs
 
-def main(ltf_file):
+def main(ltf_files):
     v = epihan.vector.VectorsWithIPASpace('cedict', 'pinyin-to-ipa', 'zh-Han')
-    tree = etree.parse(ltf_file)
-    root = tree.getroot()
-    for token in root.iter('TOKEN'):
-        print(v.word_to_segs(token.text))
+    for ltf_file in ltf_files:
+        tree = etree.parse(ltf_file)
+        root = tree.getroot()
+        for token in root.iter('TOKEN'):
+            text = unicode(token.text)
+            print('text={}'.format(text))
+            print(v.word_to_segs(text))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--input', help='Input file (LTF)')
+    parser.add_argument('-i', '--input', nargs='+', help='Input file (LTF)')
     args = parser.parse_args()
     main(args.input)
